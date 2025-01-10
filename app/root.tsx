690def71d0804a255845e7c16849827a1a63fc6c
meiki <jaj4fv9ec@icloud.com>
2025-01-10 12:23:58 +0900

    test commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,24 @@
 import {LoaderFunctionArgs} from "@remix-run/node";
+import {
+  Meta,
+  Links,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  LiveReload
+} from "@remix-run/react";
 
 const isAuthorized = (request: Request) => {
+
   const header = request.headers.get('Authorization')
   if (!header) return false
   const base64 = header.replace('Basic ', '')
   const [username, password] = Buffer.from(base64, 'base64')
     .toString()
     .split(':')
-  return username === 'admin' && password === 'password'
+
+  return username === '12345' && password === 'password'
 }
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
@@ -16,4 +27,27 @@ export const loader = ({ request }: LoaderFunctionArgs) => {
   } else {
     return { authorized: false }
   }
+}
+
+export default function App() {
+  const { authorized } = useLoaderData<typeof loader>()
+
+  if (!authorized) {
+    return <>Authorization Required</>
+  }
+
+  return (
+    <html lang="en">
+    <head>
+      <Meta />
+      <Links />
+    </head>
+    <body>
+    <Outlet />
+    <ScrollRestoration />
+    <Scripts />
+    <LiveReload />
+    </body>
+    </html>
+  )
 }
